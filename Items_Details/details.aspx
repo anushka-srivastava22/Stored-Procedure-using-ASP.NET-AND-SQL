24ba1d5dc42e5f0d1f4656bef6073f917d93a589
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2024-02-20 15:28:07 +0530

    Add files via upload

---
+++ Items_Details/details.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="details.aspx.cs" Inherits="emp_details.details" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" OnRowDataBound ="OnRowDataBound">
+            <Columns>
+                <asp:BoundField DataField="Item" HeaderText="Item" ItemStyle-Width="100" />
+                <asp:BoundField DataField="Quantity" HeaderText="Quantity" ItemStyle-Width="100" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Items_Details/details.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Drawing;
+
+namespace emp_details
+{
+    public partial class details : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.AddRange(new DataColumn[2] { new DataColumn("Item"), new DataColumn("Quantity") });
+                dt.Rows.Add("Top", 200);
+                dt.Rows.Add("Gown", 1100);
+                dt.Rows.Add("Midi", 500);
+                dt.Rows.Add("Purse", 450);
+                dt.Rows.Add("Truffle Cake", 350);
+                dt.Rows.Add("Pizza", 99);
+                dt.Rows.Add("Pasta", 140);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+        protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                TableCell cell = e.Row.Cells[1];
+                int quantity = int.Parse(cell.Text);
+                if (quantity == 0)
+                {
+                    cell.BackColor = Color.Red;
+                }
+                if (quantity > 0 && quantity <= 300)
+                {
+                    cell.BackColor = Color.Yellow;
+                }
+                if (quantity > 400 && quantity <= 1000)
+                {
+                    cell.BackColor = Color.Orange;
+                }
+            }
+        }
+    }
+    
+}
