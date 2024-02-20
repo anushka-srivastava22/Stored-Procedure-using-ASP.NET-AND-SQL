631009d8dc80cfe67a09512a5ef2d998962c6339
Anushka Srivastava <67271376+anushka-srivastava22@users.noreply.github.com>
2024-02-20 15:25:29 +0530

    Add files via upload

---
+++ Student_Details/WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Data.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <h1>Employee List</h1>
+                <asp:GridView ID="gvEmployees" runat="server"></asp:GridView>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Student_Details/WebForm1.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+           {
+              EmployeeDAL employeeDAL = new EmployeeDAL();
+              gvEmployees.DataSource = employeeDAL.GetEmployees();
+              gvEmployees.DataBind();
+           }
+        }
+    }
+    public class EmployeeDAL
+    {
+        public DataTable GetEmployees()
+        {
+            using (SqlConnection conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetEmployees", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+    }
+}
